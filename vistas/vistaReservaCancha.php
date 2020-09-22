92274f5acbfdcc0b918269d739d6a4e5ece7a6da
carlospc <carlospc@CarlosPC>
2020-09-22 00:47:34 -0500

    agregada vista reservar cancha

---
+++ vistas/vistaReservaCancha.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        //<form action="" method="post">
+            Tipo Reserva: <input type="text" name="name"><br>
+            Cantidad Personas Asistentes: <input type="email" name="email"><br>
+            Centro Deportivo: <input type="email" name="email"><br>
+            Inicio: <input type="date" name="email"><br>
+            Fin: <input type="date" name="email"><br>
+            <input type="submit">
+            </form>
+             put your code here
+        
+    </body>
+</html>
